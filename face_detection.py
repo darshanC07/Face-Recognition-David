e46b39b3f4a9b1f65afe7702dc4cb465a52bc140
Darshan Choudhary <darshanchoudhary2007@gmail.com>
2024-06-18 13:43:03 +0530

    used different way to recognize faces

--- face_detection.py
+++ face_detection.py
@@ -1,16 +1,63 @@
+import face_recognition
 import cv2
+from simple_facerec import SimpleFacerec
+import os
 
-alg = "haarcascade_frontalface_default.xml"
+sfr = SimpleFacerec()
+sfr.load_encoding_images("./known_faces/")
 
-haar_cascade = cv2.CascadeClassifier(alg)
+cap = cv2.VideoCapture(0)
 
-img = cv2.imread("/trial_img.jpg", 0)
-# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
+    ret, frame = cap.read()
+    
+    face_location, face_name = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_location, face_name):
+        # print(face_loc)
+        y1, x2, y2, x1 = face_loc[0],face_loc[1],face_loc[2],face_loc[3]
+        cv2.putText(frame, name, (x1, y1-10), cv2.FONT_HERSHEY_PLAIN,1,(0))
+        cv2.rectangle(frame,(x1,y1),(x2,y2),(0),4)
+        
+    cv2.imshow("frame",frame)
+    
+    if cv2.waitKey(1)==ord("q"):
+        break
 
-faces = haar_cascade.detectMultiScale(img, scaleFactor=1.05, minNeighbors=5, minSize=(100,100))
+cv2.destroyAllWindows()
 
-i = 0
-for x, y, w, h in faces:
-    cropped_img = img[ y:y+h, x:x+w]
-    cv2.imwrite(f"stored_face/{i}.jpg",cropped_img)
-    i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# img = cv2.imread('./images/trial_messi.jpg')
+# rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+# files = os.listdir("./known_faces")
+
+# for images in files:
+#     image = cv2.imread(f'./known_faces/{images}')
+#     rgb_img2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     img2_encoding = face_recognition.face_encodings(rgb_img2)[0]
+
+#     result = face_recognition.compare_faces([img_encoding], img2_encoding)
+#     print(result)
+    
+    
