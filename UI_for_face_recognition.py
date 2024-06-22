d8b916742846a67b6721015d73fbca2d3f380806
Darshan Choudhary <darshanchoudhary2007@gmail.com>
2024-06-22 22:53:57 +0530

    display default face screen

--- UI_for_face_recognition.py
+++ UI_for_face_recognition.py
@@ -5,13 +5,15 @@ import os
 
 cam_on = False
 cap = None
+
 mainWindow = Tk()
-mainWindow.geometry("800x650")
+mainWindow.geometry("900x1000")
 
 # Load the custom image to be displayed when the camera is off
-custom_img_path = "C:/Users/acer/OneDrive/Documents/Darshan's project/Python projects/face_recognition_system/final_face_recognition/black.png"  # Ensure this is the correct path to your custom image
+custom_img_path = "C:/Users/acer/OneDrive/Documents/Darshan's project/Python projects/face_recognition_system/final_face_recognition/face-cam.jpg"  
+
 if os.path.exists(custom_img_path):
-    custom_img = Image.open(custom_img_path).resize((810, 640))
+    custom_img = Image.open(custom_img_path).resize((822, 652))
     custom_imgtk = ImageTk.PhotoImage(custom_img)
 else:
     print(f"Error: {custom_img_path} does not exist")
@@ -22,7 +24,7 @@ def show_frame():
         ret, frame = cap.read()
         if ret:
             cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img = Image.fromarray(cv2image).resize((810, 640))
+            img = Image.fromarray(cv2image).resize((822, 652))
             imgtk = ImageTk.PhotoImage(image=img)
             vid_lbl.imgtk = imgtk
             vid_lbl.configure(image=imgtk)
@@ -30,7 +32,8 @@ def show_frame():
 
 def start_vid():
     global cam_on, cap
-    stop_vid()
+    if cam_on:
+        stop_vid()
     cam_on = True
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
@@ -47,21 +50,27 @@ def stop_vid():
     if custom_imgtk:
         vid_lbl.imgtk = custom_imgtk
         vid_lbl.configure(image=custom_imgtk)
-        print("Displaying custom image")
     else:
-        vid_lbl.configure(text="Custom image not found")
         print("Custom image not found")
 
-header_label = Label(mainWindow, width=100, height=100, anchor=CENTER)
+header_label = Label(mainWindow, width=60, height=100, anchor=CENTER)
 header_label.pack()
 
-vid_lbl = Label(mainWindow)
+vid_lbl = Label(mainWindow,highlightbackground="black",highlightthickness=4, width=800,height=650)
+
+if custom_imgtk:
+    vid_lbl.imgtk = custom_imgtk
+    vid_lbl.configure(image=custom_imgtk)
+
 vid_lbl.pack()
 
+mainWindow.update()
+# print(vid_lbl.winfo_reqwidth(),vid_lbl.winfo_reqheight())
+
 # Buttons
-TurnCameraOn = Button(header_label, text="start Video", command=start_vid, anchor=CENTER)
+TurnCameraOn = Button(header_label, text="Start Video", command=start_vid, anchor=CENTER)
 TurnCameraOn.grid(row=1, column=0, pady=20)
-TurnCameraOff = Button(header_label, text="stop Video", command=stop_vid, anchor=CENTER)
+TurnCameraOff = Button(header_label, text="Stop Video", command=stop_vid, anchor=CENTER)
 TurnCameraOff.grid(row=1, column=1, padx=20, pady=20)
 
 mainWindow.mainloop()
