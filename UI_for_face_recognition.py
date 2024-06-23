6eaea830c13199be7bea547397328faae86ce447
Darshan Choudhary <darshanchoudhary2007@gmail.com>
2024-06-23 10:12:50 +0530

    Updated UI_for_face_recognition.py

--- UI_for_face_recognition.py
+++ UI_for_face_recognition.py
@@ -74,3 +74,41 @@ TurnCameraOff = Button(header_label, text="Stop Video", command=stop_vid, anchor
 TurnCameraOff.grid(row=1, column=1, padx=20, pady=20)
 
 mainWindow.mainloop()
+
+
+#tried new design
+import tkinter as tk
+import cv2
+from PIL import ImageTk, Image
+
+IMAGE_PATH = 'assets/face_cam.png'
+
+
+# Create the main window
+root = tk.Tk()
+root.title("Main Window")
+root.geometry("1000x600")
+root.config(background="#576CA8")
+root.grid_columnconfigure(0, minsize=600)
+root.grid_columnconfigure(1, minsize=367)
+root.grid_rowconfigure(0)
+root.grid_rowconfigure(1, weight=1)
+
+header = tk.Label(root,text="Face-Recognition System")
+header.grid(row=0,columnspan=2,pady=10)
+# Create and place the frames
+frame1 = tk.Frame(root, bg='red',width=600,height=500)
+frame2 = tk.Frame(root, bg='green',width=367,height=500)
+
+camera_screen = tk.PhotoImage(file = IMAGE_PATH)
+canvas_face_cam = tk.Canvas(frame1, width = 300, height = 300)
+canvas_face_cam.pack()
+canvas_face_cam.create_image((0, 0), image = camera_screen)
+frame1.grid(row=1, column=0, sticky="nsew", padx=(5, 10))
+frame2.grid(row=1, column=1, sticky="nsew", padx=(10, 5))
+
+
+
+
+# Run the main loop
+root.mainloop()
