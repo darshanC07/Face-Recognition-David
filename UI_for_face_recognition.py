821154a614bb1f94288b92a3e121030626f5fc27
Darshan Choudhary <darshanchoudhary2007@gmail.com>
2024-06-26 14:15:42 +0530

    added curved border bg to frame 2

--- UI_for_face_recognition.py
+++ UI_for_face_recognition.py
@@ -52,16 +52,19 @@ root.grid_rowconfigure(0)
 root.grid_rowconfigure(1, weight=1)
 
 camera_screen = tk.PhotoImage(file = IMAGE_PATH).subsample(1)
+rounded_frame2_bg = tk.PhotoImage(file = "./assets/frame2bg.png")
 
 header = tk.Label(root,text="Face-Recognition System")
 header.grid(row=0,columnspan=2,pady=10)
 
 # Create and place the frames
 frame1 = tk.Frame(root, bg='#576CA8',width=600,height=500)
-frame2 = tk.Frame(root, bg='green',width=367,height=500)
+frame2 = tk.Canvas(root, bg='#576CA8',width=367,height=500,borderwidth=0, highlightthickness=0)
 
-canvas_face_cam = tk.Canvas(frame1, width = 600, height = 300)
-canvas_face_cam.create_image((302,150), image = camera_screen)
+frame2.create_image((185,250),image = rounded_frame2_bg)
+
+canvas_face_cam = tk.Canvas(frame1, width = 600, height = 300,borderwidth=0, highlightthickness=0)
+canvas_face_cam.create_image((300,150), image = camera_screen)
 canvas_face_cam.pack(anchor="center",pady=100,fill=tk.BOTH)
 
 #buttons 
@@ -70,8 +73,8 @@ TurnCameraOn.place(anchor="center",x=250,y= 425)
 TurnCameraOff = tk.Button(frame1, text="Stop Video", command=stop_vid, anchor="center")
 TurnCameraOff.place(anchor="center",x=350,y= 425)
 
-frame1.grid(row=1, column=0, sticky="nsew", padx=(5, 10))
-frame2.grid(row=1, column=1, sticky="nsew", padx=(10, 5))
+frame1.grid(row=1, column=0, sticky="nsew", padx=(8, 8))
+frame2.grid(row=1, column=1, sticky="nsew", padx=(8, 8))
 
 
 
