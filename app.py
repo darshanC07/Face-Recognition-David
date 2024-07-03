aed90e58b836e4f1b32bbe3f7b2eca31190de8a4
Darshan Choudhary <darshanchoudhary2007@gmail.com>
2024-07-03 12:58:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,47 @@ from config import GEMINI_API
 import speech_recognition as sr
 
 IMAGE_PATH = './assets/face_cam.png'
+USER_INPUT = None
+AI_RESPONSE = None
 cam_on = False
 cap = None
 count = 0
 current_y = 170  # Starting y-coordinate for the first text message
 unknown_face_dict = {}
+conversation = []
 recognizer = sr.Recognizer()
-
 with open("./known_face_encoding.json") as file:
     known_faces_encodings = json.load(file)
     if known_faces_encodings is None:
         known_faces_encodings = {}
+
+
+          
+          
+def ai_model(prompt):
+    global GEMINI_API
+    genai.configure(api_key=GEMINI_API)
+    
+    generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 0,
+    "max_output_tokens": 2048,
+    "response_mime_type": "text/plain",
+    }
+
+    model = genai.GenerativeModel(
+    model_name="gemini-1.5-pro",
+    generation_config=generation_config,
+    system_instruction=f"you are david, a virtual assistant, built by 'Darshan Choudhary'. you are provided with conversation history {conversation} and your work is to go through the history and analyze it. if your input prompt is available in history then answer the input from conversation history and if the question is new to you then answer how you normally answer. answers should be to the point and short. act friendly with user, give polite replies and act similar to humans",
+    )
+    
+    
+    chat_session = model.start_chat()
+
+    response = chat_session.send_message(prompt)
+    
+    return response.text
 
 
 def voice_command():
@@ -32,37 +62,15 @@ def voice_command():
             text_adder(text,role="User")
             ai_response = ai_model(text)
             text_adder(ai_response,role="David")
+            conversation.append({"user": text, "david": ai_response})
             
     except sr.UnknownValueError:
         print("Sorry, could not understand audio.")
         
     except sr.RequestError as e:
         print("Error: Could not request results from Google Speech Recognition service")
-          
-          
-def ai_model(prompt):
-    genai.configure(api_key=GEMINI_API)
-    
-    generation_config = {
-    "temperature": 0.9,
-    "top_p": 1,
-    "top_k": 0,
-    "max_output_tokens": 2048,
-    "response_mime_type": "text/plain",
-    }
-
-    model = genai.GenerativeModel(
-    model_name="gemini-1.0-pro",
-    generation_config=generation_config,
-    )
-    
-    chat_session = model.start_chat()
-
-    response = chat_session.send_message(prompt)
-    
-    return response.text
-
-
+        
+        
 def text_adder(text=True, role=None):
     global current_y
     if role:
@@ -211,10 +219,10 @@ text_input = tk.Entry(frame2,width=45)
 text_input.place(x=20, y=450)
 
 #adding voice command button 
-voice_img = Image.open("./assets/voice_recorder.png").resize((20,20))
-resized_voice_img = ImageTk.PhotoImage(image=voice_img)
+voice_img = Image.open("./assets/microphone.png").resize((20,20))
+resized_microphone_img = ImageTk.PhotoImage(image=voice_img)
 
-button = tk.Button(frame2,command=voice_command,image=resized_voice_img,background="#274690",borderwidth=0)
+button = tk.Button(frame2,command=voice_command,image=resized_microphone_img,background="#274690",borderwidth=0)
 button.place(x=305, y=448)
 
 #adding send button
@@ -244,5 +252,87 @@ enableFaceRecognition.place(anchor="center", x=450, y=460)
 frame1.grid(row=1, column=0, sticky="nsew", padx=(8, 8))
 frame2.grid(row=1, column=1, sticky="nsew", padx=(8, 8))
 
+while True:
+        print("listening..")
+        try: 
+            command = takeCommand().lower()
+            print(f"User : {command}")
+
+            if "quit" in command :
+                print("OK Thank You Sir , Have a nice day")
+                say("Ok Thank You Sir , Hve a nice day")
+                exit()
+                
+            if command=="david":
+                continue
+            
+            category = classifier(command)
+            
+            if "command".lower() in category:
+                print("Working on it Sir.")
+                say("Working on it Sir.")
+                
+                new_category = new_classifier(command)
+                # print(new_category)
+                
+                if "clear conversation history".lower() in command:
+                    conversation = []  
+                    
+                elif "conversation history".lower() in command :
+                    # print("here")
+                    print(conversation)
+                                
+                elif "other".lower() in new_category:
+                    # print("here")
+                    new_prompt = f"just return the code which will fulfill the command , just return the code of following command and nothing else , mostly use os module for it,but if not suitable then you can also use webbrowser module to open sites and consider that os module is already imported: {command} consider that the device is of Windows OS"
+                    code_response = gemini_ai(new_prompt)
+                    final_code = cleaner(code_response)
+                    conversation_appender(command,final_code)
+                    # print(final_code)
+                    exec(final_code)
+                    
+                else:
+                    # print("in command")
+                    chat_response = chat(command)
+                    conversation_appender(command,chat_response)
+                    print(f"Role : David\nContent : {chat_response}")
+                    say(chat_response)
+                   
+            elif "question".lower() in category:
+                
+                if "time" in command :
+                    hour = int(datetime.datetime.now().strftime("%H"))
+                    if hour>12:
+                        hour = hour - 12
+                    mins = int(datetime.datetime.now().strftime("%H"))
+                    print(f"Current time is {hour} hours {mins} minutes")
+                    say(f"Current time is {hour} hours {mins} minutes")
+                    
+                else:
+                    # print("in question")
+                    chat_response = chat(command)
+                    conversation_appender(command,chat_response)
+                    print(f"Role : David\nContent : {chat_response}")
+                    say(chat_response)
+                        
+                  
+            elif "artificial".lower() in command :
+                prompt = command.split("intelligence",1)[1]
+                response = gemini_ai(prompt)
+                conversation_appender(command,chat_response)
+                print(response)
+              
+            else:
+                # print("in else")
+                chat_response = chat(command)
+                conversation_appender(command,chat_response)
+                print(f"Role : David\nContent : {chat_response}")
+                say(chat_response)
+                
+                
+        except Exception as e:
+            # print(e)
+            pass
+        
 # Run the main loop
 root.mainloop()
